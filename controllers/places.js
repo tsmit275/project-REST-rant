c72e2c4891b6aa83d98e3c4c694646a5c85e1628
tsmit275 <taylor.lsmith03@gmail.com$ git config --global user.taylor.lsmith03@gmail.com>
2023-11-01 22:30:00 -0400

    Adding default view for layout

--- controllers/places.js
+++ controllers/places.js
@@ -4,4 +4,42 @@ router.get('/', (req, res) => {
     res.send('GET /places')
 })
 
+// router.post('/places', (req, res) => {
+//    res.send('POST /places')
+// })
+
+// //Commenting out all of this code below
+
+// router.get('/places/new', (req, res) => {
+//    res.send('GET /places/new')
+// })
+
+// router.get('/places/:id', (req, res) => {
+//    res.send('GET /places/:id')
+// })
+
+// router.post('/places/:id', (req, res) => {
+//    res.send('POST /places/:id')
+// })
+
+// router.get('/places/:id/edit', (req, res) => {
+//    res.send('GET /place/:id/edit')
+// })
+
+// router.delete('/places/:id', (req, res) => {
+//    res.send('DELETE /places/:id')
+// })
+
+//  router.post('/places/:id/rant', (req, res) => {
+//    res.send('POST /places/:id/rant')
+//  })
+
+//  router.delete('/places/:id/rant/:rantId', (req, res) => {
+//    res.send('DELETE /places/:id/rant/:rantId')
+//  })
+
+//  router.get('*', (req,res) => {
+//    res.status(404).send('<h1>404 Page</h1>')
+//  })
+
 module.exports = router 
